e440e083aa973e6c7dc5326086f1ec276a4b9b7e
Ari Pratomo <ari633@gmail.com>
2013-02-26 00:56:14 +0700

    Update

--- gen/com/tugasakhir/twitclient/R.java
+++ gen/com/tugasakhir/twitclient/R.java
@@ -22,21 +22,22 @@ public final class R {
         public static final int home=0x7f020002;
         public static final int home_grey=0x7f020003;
         public static final int home_normal=0x7f020004;
-        public static final int homebtnbg=0x7f020005;
-        public static final int ic_action_search=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int ic_tab_favorite=0x7f020008;
-        public static final int ic_tab_home=0x7f020009;
-        public static final int ic_tab_interaksi=0x7f02000a;
-        public static final int ic_tab_message=0x7f02000b;
-        public static final int interaksi_normal=0x7f02000c;
-        public static final int intereksi_grey=0x7f02000d;
-        public static final int message_grey=0x7f02000e;
-        public static final int message_normal=0x7f02000f;
-        public static final int profilebg=0x7f020010;
-        public static final int tweet=0x7f020011;
-        public static final int tweetbtnbg=0x7f020012;
-        public static final int updatebtnbg=0x7f020013;
+        public static final int homebg=0x7f020005;
+        public static final int homebtnbg=0x7f020006;
+        public static final int ic_action_search=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int ic_tab_favorite=0x7f020009;
+        public static final int ic_tab_home=0x7f02000a;
+        public static final int ic_tab_interaksi=0x7f02000b;
+        public static final int ic_tab_message=0x7f02000c;
+        public static final int interaksi_normal=0x7f02000d;
+        public static final int intereksi_grey=0x7f02000e;
+        public static final int message_grey=0x7f02000f;
+        public static final int message_normal=0x7f020010;
+        public static final int profilebg=0x7f020011;
+        public static final int tweet=0x7f020012;
+        public static final int tweetbtnbg=0x7f020013;
+        public static final int updatebtnbg=0x7f020014;
     }
     public static final class id {
         public static final int dotweet=0x7f080005;
