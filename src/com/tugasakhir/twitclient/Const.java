3a3d98af81fc39a28c0232df5790f4521c77f248
Ari <ari633@gmail.com>
2013-10-31 01:31:12 +0700

    Update

--- src/com/tugasakhir/twitclient/Const.java
+++ src/com/tugasakhir/twitclient/Const.java
@@ -3,8 +3,8 @@ package com.tugasakhir.twitclient;
 public final class Const {
 	
 	/**developer account key for this app*/
-	public final static String TWIT_KEY = "5fEpQsTRIa24fK2eap4lA";   
+	public final static String TWIT_KEY = "YOUR KEY";   
 	/**developer secret for the app*/
-	public final static String TWIT_SECRET = "DPfJ0Cf34Nz0CpvUSPGiWABvz6vYeKnF4zeql2go"; 
+	public final static String TWIT_SECRET = "YOUR SECRET"; 
 
 }
